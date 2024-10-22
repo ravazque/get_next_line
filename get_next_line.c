59343f6aaa220642b095a1033bb0a899aec03df6
Raul <rk.raul1306@gmail.com>
2024-10-22 16:54:03 +0200

    proyectDone - noBonus

---
+++ get_next_line.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 22:38:40 by ravazque          #+#    #+#             */
+/*   Updated: 2024/10/22 14:47:09 by ravazque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*extract_line(char *buffer)
+{
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		line = (char *)malloc(i + 2);
+	else
+		line = (char *)malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*update_buffer(char *buffer)
+{
+	char	*new_buffer;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	new_buffer = (char *)malloc(ft_strlen(buffer) - i);
+	if (!new_buffer)
+		return (NULL);
+	i++;
+	j = 0;
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+	char		*tmp_buffer;
+	ssize_t		bytes_read;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	tmp_buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!tmp_buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(buffer, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, tmp_buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+			return (free(tmp_buffer), free(buffer), buffer = NULL, NULL);
+		tmp_buffer[bytes_read] = '\0';
+		buffer = ft_strjoin(buffer, tmp_buffer);
+		if (!buffer)
+			return (free(tmp_buffer), (NULL));
+	}
+	free(tmp_buffer);
+	if (!buffer || *buffer == '\0')
+		return (free(buffer), buffer = NULL, NULL);
+	return (line = extract_line(buffer), buffer = update_buffer(buffer), line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 16:28:50 by ravazque          #+#    #+#             */
+/*   Updated: 2024/10/22 12:38:55 by ravazque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+size_t		ft_strlen(const char *str);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strjoin(char *s1, char *s2);
+
+char		*get_next_line(int fd);
+
+#endif
