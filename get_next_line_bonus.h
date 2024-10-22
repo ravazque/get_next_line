28aa62f46c12d11ec94ac2e2e8582a546c86fde3
Raul <rk.raul1306@gmail.com>
2024-10-22 18:51:41 +0200

    projectDone - Bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ravazque <ravazque@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 16:28:50 by ravazque          #+#    #+#             */
-/*   Updated: 2024/10/22 18:41:21 by ravazque         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:26:06 by ravazque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -21,10 +20,11 @@
 #  define BUFFER_SIZE 10
 # endif
 
-size_t		ft_strlen(const char *str);
-char		*ft_strchr(const char *s, int c);
-char		*ft_strjoin(char *s1, char *s2);
+# define MAX_FD 1024
 
-char		*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 
 #endif
